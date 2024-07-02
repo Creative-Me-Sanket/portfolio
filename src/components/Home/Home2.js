9c499131f404d9075dde8270fade9cfc108b2f76
Sanket Khule <sanket.khule@equip9.com>
2024-07-03 01:31:31 +0530

    portfolio version 1

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+           I'm a passionate software developer and technology enthusiast. 
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple"> Java and Javascript. </b>
+              </i>
+              <br />
+              <br />
+              I love exploring the world of technology and building  &nbsp;
+              <i>
+                <b className="purple">innovative products </b> 
+              </i>
+              <br />
+              <br />
+              With over two years of experience in the industry, I have honed my skills in developing complex &nbsp;
+                <i>
+              <b className="purple">
+                back-end and front-end</b>  web applications.
+                  </i>
+             <br />
+             <br />
+             In addition to my tech pursuits, I am also an avid  &nbsp;
+             <i>
+              <b className="purple">
+              guitarist
+              </b>
+              </i> and enjoy teaching guitar in my spare time.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Creative-Me-Sanket"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/sanket_khule"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://linkedin.com/in/sanket-khule-47213499"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/sanket_khule21"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
