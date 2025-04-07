1f2d90b48854bb9716178aaa30ab2fef27d51e7d
Creative-Me-Sanket <khule.sanket39396@gmail.com>
2025-04-07 10:19:46 +0530

    updated-pagination

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -10,7 +10,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
-  const pages = [1,2,3]
+  const pages = [1]
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
